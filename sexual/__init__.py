3809bbe107eb041f329701ad437a5937e13bb55e
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-17 16:16:34 +0800

    Create __init__.py

---
+++ sexual/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+=============================================================================
+SEXUAL PACKAGE
+=============================================================================
+Konten dewasa untuk dinamika hubungan intim
+⚠️ Hanya untuk pengguna 18+
+"""
+
+from sexual.positions import SexPosition, POSITIONS
+from sexual.areas import SensitiveArea, AREAS
+from sexual.activities import SexualActivity, ACTIVITIES
+from sexual.climax import ClimaxVariation, CLIMAX_VARIATIONS
+from sexual.dynamics import SexualDynamics
+
+
+__all__ = [
+    'SexPosition',
+    'POSITIONS',
+    'SensitiveArea',
+    'AREAS',
+    'SexualActivity',
+    'ACTIVITIES',
+    'ClimaxVariation',
+    'CLIMAX_VARIATIONS',
+    'SexualDynamics'
+]
