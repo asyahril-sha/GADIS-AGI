9f15378d87d506afa83346136467eac283630d10
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-17 17:34:27 +0800

    Create __init__.py

---
+++ systems/__init__.py
@@ -0,0 +1,34 @@
+"""
+SYSTEMS PACKAGE - Inisialisasi semua systems
+"""
+
+from systems.hts_fwb_system import HTSFWBSystem, RankingSystem
+from systems.role_archetypes import (
+    RoleFactory,
+    RoleArchetype,
+    MantanArchetype,
+    TemanSMAArchetype,
+    JandaArchetype,
+    PelakorArchetype,
+    IstriOrangArchetype,
+    SepupuArchetype,
+    IparArchetype,
+    TemanKantorArchetype,
+    PDKTArchetype
+)
+
+__all__ = [
+    'HTSFWBSystem',
+    'RankingSystem',
+    'RoleFactory',
+    'RoleArchetype',
+    'MantanArchetype',
+    'TemanSMAArchetype',
+    'JandaArchetype',
+    'PelakorArchetype',
+    'IstriOrangArchetype',
+    'SepupuArchetype',
+    'IparArchetype',
+    'TemanKantorArchetype',
+    'PDKTArchetype'
+]
