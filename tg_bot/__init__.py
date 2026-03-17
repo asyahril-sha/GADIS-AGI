04197aab4ce78d6ef2ed853faa2260191bc5cedf
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-17 19:49:00 +0800

    Create __init__.py

---
+++ tg_bot/__init__.py
@@ -0,0 +1,11 @@
+"""
+TELEGRAM PACKAGE - Inisialisasi semua handlers
+"""
+
+from telegram.handlers import TelegramHandlers
+from telegram.commands import AdditionalCommands
+
+__all__ = [
+    'TelegramHandlers',
+    'AdditionalCommands'
+]
